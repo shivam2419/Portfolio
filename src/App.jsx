f6bcd1d276693eb1e3857766c061a6b46d441606
shivam2419 <shivam241980@gmail.com>
2024-10-30 15:09:22 +0530

    New components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Navbar from './components/navbar'
-import Hero from './components/hero'
-import About from './components/about'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
 import Technologies from './components/Technologies'
 import Experience from './components/Experience'
-import Projects from './components/projects'
-import Contact from './components/contact'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
 const App = () => {
   return (
     <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
