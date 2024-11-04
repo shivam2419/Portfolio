cc974acbfe479254cf7fba114c79c01e63128202
Shivam Sharma <120017051+shivam2419@users.noreply.github.com>
2024-11-05 00:14:48 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import {HERO_CONTENT} from "../constants";
-import profilePic from "../assets/kevinRushProfile.jpg";
+import profilePic from "../assets/ShivamSharmaprofile.png";
 import { motion } from "framer-motion"
 const container = (delay) => ({
   hidden : {x : -100, opacity: 0},
