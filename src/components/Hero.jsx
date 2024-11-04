ba6b8d68081e6bbbaef47f43255e188995156c05
shivam2419 <shivam241980@gmail.com>
2024-11-05 00:12:13 +0530

    changed image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import {HERO_CONTENT} from "../constants";
-import profilePic from "../assets/ShivamSharmaProfile.png";
+import profilePic from "../assets/kevinRushProfile.jpg";
 import { motion } from "framer-motion"
 const container = (delay) => ({
   hidden : {x : -100, opacity: 0},
